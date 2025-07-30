f9e26176020995e22c5b4ebe079c362c044d408e
李智 <lizh@ruit.cc>
2025-07-30 11:43:21 +0800

    1.安卓相关修改

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,14 @@ class HtmlEditorExampleApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(),
       darkTheme: ThemeData.dark(),
-      home: HtmlEditorExample(title: 'Flutter HTML Editor Example'),
+      locale: const Locale('ar'), // 设置语言为阿拉伯语
+      builder: (context, child) {
+        return Directionality(
+          textDirection: TextDirection.rtl, // 设置从右到左方向
+          child: child!,
+        );
+      },
+      home: HtmlEditorExample(title: 'Flutter HTML123 Editor Example'),
     );
   }
 }
@@ -33,18 +40,20 @@ class _HtmlEditorExampleState extends State<HtmlEditorExample> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        if (!kIsWeb) {
-          controller.clearFocus();
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          elevation: 0,
-          actions: [
-            IconButton(
+    return Directionality(
+      textDirection: Directionality.of(context), // 强制 RTL
+      child: GestureDetector(
+        onTap: () {
+          if (!kIsWeb) {
+            controller.clearFocus();
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            elevation: 0,
+            actions: [
+              IconButton(
                 icon: Icon(Icons.refresh),
                 onPressed: () {
                   if (kIsWeb) {
@@ -52,121 +61,258 @@ class _HtmlEditorExampleState extends State<HtmlEditorExample> {
                   } else {
                     controller.editorController!.reload();
                   }
-                })
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            controller.toggleCodeView();
-          },
-          child: Text(r'<\>',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              HtmlEditor(
-                controller: controller,
-                htmlEditorOptions: HtmlEditorOptions(
-                  hint: 'Your text here...',
-                  shouldEnsureVisible: true,
-                  //initialText: "<p>text content initial, if any</p>",
-                ),
-                htmlToolbarOptions: HtmlToolbarOptions(
-                  toolbarPosition: ToolbarPosition.belowEditor,
-                  //by default
-                  toolbarType: ToolbarType.nativeGrid,
-                  //by default
-                  defaultToolbarButtons: [
-                    FontButtons(
-                      bold: true,
-                      italic: true,
-                      underline: false,
-                      clearAll: false,
-                      strikethrough: false,
-                      superscript: false,
-                      subscript: false,
-                    ),
-                    OtherButtons(
-                      fullscreen: false,
-                      codeview: false,
-                      undo: true,
-                      redo: true,
-                      help: false,
-                      copy: false,
-                      paste: false,
-                    )
-                  ],
-                  onButtonPressed:
-                      (ButtonType type, bool? status, Function? updateStatus) {
-                    print(
-                        "button '${describeEnum(type)}' pressed, the current selected status is $status");
-                    return true;
-                  },
-                  onDropdownChanged: (DropdownType type, dynamic changed,
-                      Function(dynamic)? updateSelectedItem) {
-                    print(
-                        "dropdown '${describeEnum(type)}' changed to $changed");
-                    return true;
-                  },
-                  mediaLinkInsertInterceptor:
-                      (String url, InsertFileType type) {
-                    print(url);
-                    return true;
-                  },
-                ),
-                otherOptions: OtherOptions(height: 580),
-                callbacks: Callbacks(onBeforeCommand: (String? currentHtml) {
-                  print('html before change is $currentHtml');
-                }, onChangeContent: (String? changed) {
-                  print('content changed to $changed');
-                }, onChangeCodeview: (String? changed) {
-                  print('code changed to $changed');
-                }, onChangeSelection: (EditorSettings settings) {
-                  print('parent element is ${settings.parentElement}');
-                  print('font name is ${settings.fontName}');
-                }, onDialogShown: () {
-                  print('dialog shown');
-                }, onEnter: () {
-                  print('enter/return pressed');
-                }, onFocus: () {
-                  print('editor focused');
-                }, onBlur: () {
-                  print('editor unfocused');
-                }, onBlurCodeview: () {
-                  print('codeview either focused or unfocused');
-                }, onInit: () {
-                  print('init');
-                }, onImageUploadError:
-                    (FileUpload? file, String? base64Str, UploadError error) {
-                  print(describeEnum(error));
-                  print(base64Str ?? '');
-                  if (file != null) {
-                    print(file.name);
-                    print(file.size);
-                    print(file.type);
-                  }
-                }, onKeyDown: (int? keyCode) {
-                  print('$keyCode key downed');
-                  print(
-                      'current character count: ${controller.characterCount}');
-                }, onKeyUp: (int? keyCode) {
-                  print('$keyCode key released');
-                }, onMouseDown: () {
-                  print('mouse downed');
-                }, onMouseUp: () {
-                  print('mouse released');
-                }, onNavigationRequestMobile: (String url) {
-                  print(url);
-                  return NavigationActionPolicy.ALLOW;
-                }, onPaste: () {
-                  print('pasted into editor');
-                }, onScroll: () {
-                  print('editor scrolled');
-                }),
+                },
               ),
             ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              controller.toggleCodeView();
+            },
+            child: Text(r'<\>',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          body: Container(
+            color: Color(0xFFF7F7F7),
+            margin: EdgeInsets.fromLTRB(15, 15, 15, 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 150,),
+                HtmlEditor(
+                  controller: controller,
+                  htmlEditorOptions: HtmlEditorOptions(
+                    hint:'1. متاح على تطبيق الق من إيرادات القراءة والمكافآت.',
+                    shouldEnsureVisible: true,
+                  ),
+                  htmlToolbarOptions: HtmlToolbarOptions(
+                    toolbarPosition: ToolbarPosition.belowEditor,
+                    toolbarType: ToolbarType.nativeGrid,
+                    defaultToolbarButtons: [
+                      FontButtons(
+                        bold: true,
+                        italic: true,
+                        underline: false,
+                        clearAll: false,
+                        strikethrough: false,
+                        superscript: false,
+                        subscript: false,
+                      ),
+                      OtherButtons(
+                        fullscreen: false,
+                        codeview: false,
+                        undo: true,
+                        redo: true,
+                        help: false,
+                        copy: false,
+                        paste: false,
+                      ),
+                    ],
+                    onButtonPressed: (type, status, updateStatus) {
+                      print(
+                          "button '${describeEnum(type)}' pressed, the current selected status is $status");
+                      return true;
+                    },
+                    onDropdownChanged: (type, changed, updateSelectedItem) {
+                      print(
+                          "dropdown '${describeEnum(type)}' changed to $changed");
+                      return true;
+                    },
+                    mediaLinkInsertInterceptor: (url, type) {
+                      print(url);
+                      return true;
+                    },
+                  ),
+                  otherOptions: OtherOptions(height: 580),
+                  callbacks: Callbacks(
+                    onBeforeCommand: (html) {
+                      print('html before change is $html');
+                    },
+                    onChangeContent: (changed) {
+                      print('content changed to $changed');
+                    },
+                    onChangeCodeview: (changed) {
+                      print('code changed to $changed');
+                    },
+                    onChangeSelection: (settings) {
+                      print('parent element is ${settings.parentElement}');
+                      print('font name is ${settings.fontName}');
+                    },
+                    onDialogShown: () {
+                      print('dialog shown');
+                    },
+                    onEnter: () {
+                      print('enter/return pressed');
+                    },
+                    onFocus: () {
+                      print('editor focused');
+                    },
+                    onBlur: () {
+                      print('editor unfocused');
+                    },
+                    onBlurCodeview: () {
+                      print('codeview either focused or unfocused');
+                    },
+                    onInit: () {
+                      print('init');
+                    },
+                    onImageUploadError: (file, base64Str, error) {
+                      print(describeEnum(error));
+                      print(base64Str ?? '');
+                      if (file != null) {
+                        print(file.name);
+                        print(file.size);
+                        print(file.type);
+                      }
+                    },
+                    onKeyDown: (keyCode) {
+                      print('$keyCode key downed');
+                      print(
+                          'current character count: ${controller.characterCount}');
+                    },
+                    onKeyUp: (keyCode) {
+                      print('$keyCode key released');
+                    },
+                    onMouseDown: () {
+                      print('mouse downed');
+                    },
+                    onMouseUp: () {
+                      print('mouse released');
+                    },
+                    onNavigationRequestMobile: (url) {
+                      print(url);
+                      return NavigationActionPolicy.ALLOW;
+                    },
+                    onPaste: () {
+                      print('pasted into editor');
+                    },
+                    onScroll: () {
+                      print('editor scrolled');
+                    },
+                  ),
+                ),
+              ],
+            ),
+            // child: SingleChildScrollView(
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: <Widget>[
+            //       // Container(height: 300,),
+            //       HtmlEditor(
+            //         controller: controller,
+            //         htmlEditorOptions: HtmlEditorOptions(
+            //           hint:'1. متاح على تطبيق الق من إيرادات القراءة والمكافآت.',
+            //           shouldEnsureVisible: true,
+            //         ),
+            //         htmlToolbarOptions: HtmlToolbarOptions(
+            //           toolbarPosition: ToolbarPosition.belowEditor,
+            //           toolbarType: ToolbarType.nativeGrid,
+            //           defaultToolbarButtons: [
+            //             FontButtons(
+            //               bold: true,
+            //               italic: true,
+            //               underline: false,
+            //               clearAll: false,
+            //               strikethrough: false,
+            //               superscript: false,
+            //               subscript: false,
+            //             ),
+            //             OtherButtons(
+            //               fullscreen: false,
+            //               codeview: false,
+            //               undo: true,
+            //               redo: true,
+            //               help: false,
+            //               copy: false,
+            //               paste: false,
+            //             ),
+            //           ],
+            //           onButtonPressed: (type, status, updateStatus) {
+            //             print(
+            //                 "button '${describeEnum(type)}' pressed, the current selected status is $status");
+            //             return true;
+            //           },
+            //           onDropdownChanged: (type, changed, updateSelectedItem) {
+            //             print(
+            //                 "dropdown '${describeEnum(type)}' changed to $changed");
+            //             return true;
+            //           },
+            //           mediaLinkInsertInterceptor: (url, type) {
+            //             print(url);
+            //             return true;
+            //           },
+            //         ),
+            //         otherOptions: OtherOptions(height: 580),
+            //         callbacks: Callbacks(
+            //           onBeforeCommand: (html) {
+            //             print('html before change is $html');
+            //           },
+            //           onChangeContent: (changed) {
+            //             print('content changed to $changed');
+            //           },
+            //           onChangeCodeview: (changed) {
+            //             print('code changed to $changed');
+            //           },
+            //           onChangeSelection: (settings) {
+            //             print('parent element is ${settings.parentElement}');
+            //             print('font name is ${settings.fontName}');
+            //           },
+            //           onDialogShown: () {
+            //             print('dialog shown');
+            //           },
+            //           onEnter: () {
+            //             print('enter/return pressed');
+            //           },
+            //           onFocus: () {
+            //             print('editor focused');
+            //           },
+            //           onBlur: () {
+            //             print('editor unfocused');
+            //           },
+            //           onBlurCodeview: () {
+            //             print('codeview either focused or unfocused');
+            //           },
+            //           onInit: () {
+            //             print('init');
+            //           },
+            //           onImageUploadError: (file, base64Str, error) {
+            //             print(describeEnum(error));
+            //             print(base64Str ?? '');
+            //             if (file != null) {
+            //               print(file.name);
+            //               print(file.size);
+            //               print(file.type);
+            //             }
+            //           },
+            //           onKeyDown: (keyCode) {
+            //             print('$keyCode key downed');
+            //             print(
+            //                 'current character count: ${controller.characterCount}');
+            //           },
+            //           onKeyUp: (keyCode) {
+            //             print('$keyCode key released');
+            //           },
+            //           onMouseDown: () {
+            //             print('mouse downed');
+            //           },
+            //           onMouseUp: () {
+            //             print('mouse released');
+            //           },
+            //           onNavigationRequestMobile: (url) {
+            //             print(url);
+            //             return NavigationActionPolicy.ALLOW;
+            //           },
+            //           onPaste: () {
+            //             print('pasted into editor');
+            //           },
+            //           onScroll: () {
+            //             print('editor scrolled');
+            //           },
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ),
         ),
       ),
